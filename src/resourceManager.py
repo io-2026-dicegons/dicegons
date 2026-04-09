1961edd45ce82f0d82ae43c1acaa8f7f191b1e5f
Marcin Regdos <11410592+mregdos@users.noreply.github.com>
2026-04-10 01:26:14 +0200

    add some basic tests for ResourceManager

---
+++ src/resourceManager.py
@@ -0,0 +1,36 @@
+
+class ResourceManager:
+    def __init__(self):
+        pass
+
+    def load_definitions(self):
+        # load units, building, terrain types from appropriate files
+        pass
+
+    def load_unit_types(self, file_path):
+        pass
+        
+    def load_building_types(self, file_path):
+        pass
+    
+    def load_terrain_types(self, file_path):
+        pass
+            
+    def get_unit_types(self):
+        pass
+
+    def get_unit_by_id(self, unit_id):
+        pass
+    
+    def get_building_types(self):
+        pass
+
+    def get_building_by_id(self, b_id):
+        pass
+        
+    def get_terrain_types(self):
+        pass
+
+    def get_terrain_by_id(self, t_id):
+        pass       
+
---
+++ tests/test_ResourceManager.py
@@ -0,0 +1,174 @@
+import pytest
+
+from resourceManager import ResourceManager
+from pathlib import Path
+
+def test_load_single_unit (tmp_path):
+    file = tmp_path / "units.json"
+
+    file.write_text("""
+    {
+      "unit_types": [
+        {
+          "ID_Typu_Jednostki": 1,
+          "name": "Peasant",
+          "Atak_Kostka": 2,
+          "Atak_Modifier": 0,
+          "Obrona_Kostka": 2,
+          "Obrona_Modifier": 0,
+          "Max_Stos": 100,
+          "Price": 50,
+          "Max_Obronny_Rzut": 2,
+          "Min_Obronny_Rzut": 0,
+          "Max_Atakujacy_Rzut": 2,
+          "Min_Atakujacy_Rzut": 0
+        }
+      ]
+    }
+    """)
+
+    rm = ResourceManager()
+    rm.load_unit_types(file)
+    assert rm.get_unit_types() != None
+
+    assert len(rm.get_unit_types()) == 1
+    loaded_unit = rm.get_unit_by_id(1)
+    
+    assert loaded_unit.GetName() == "Peasant"
+    assert loaded_unit.GetID() == 1
+    assert loaded_unit.GetAttackDice() == 2
+    assert loaded_unit.GetAttackModifier() == 0
+    assert loaded_unit.GetDefenseDice() == 2
+    assert loaded_unit.GetDefenseModifier() == 0
+    assert loaded_unit.GetMaxQuantity() == 100
+    assert loaded_unit.GetPrice() == 50
+    
+
+def test_load_several_units (tmp_path):
+    file = tmp_path / "units.json"
+
+    file.write_text("""
+    {
+      "unit_types": [
+        {
+          "ID_Typu_Jednostki": 1,
+          "name": "Peasant",
+          "Atak_Kostka": 3,
+          "Atak_Modifier": 3,
+          "Obrona_Kostka": 3,
+          "Obrona_Modifier": 3,
+          "Max_Stos": 300,
+          "Price": 30,
+          "Max_Obronny_Rzut": 6,
+          "Min_Obronny_Rzut": 3,
+          "Max_Atakujacy_Rzut": 6,
+          "Min_Atakujacy_Rzut": 3
+        },
+        {
+          "ID_Typu_Jednostki": 2,
+          "name": "Peasant",
+          "Atak_Kostka": 2,
+          "Atak_Modifier": 0,
+          "Obrona_Kostka": 2,
+          "Obrona_Modifier": 0,
+          "Max_Stos": 100,
+          "Price": 50,
+          "Max_Obronny_Rzut": 2,
+          "Min_Obronny_Rzut": 0,
+          "Max_Atakujacy_Rzut": 2,
+          "Min_Atakujacy_Rzut": 0
+        }
+        
+      ]
+    }
+    """)
+
+    rm = ResourceManager()
+    rm.load_unit_types(file)
+    pass
+    assert rm.get_unit_types() != None
+    assert len(rm.get_unit_types()) == 2
+    
+    loaded_unit = rm.get_unit_by_id(2)
+    assert loaded_unit.GetName() == "Peasant"
+    assert loaded_unit.GetID() == 1
+    assert loaded_unit.GetAttackDice() == 3
+    assert loaded_unit.GetAttackModifier() == 3
+    assert loaded_unit.GetDefenseDice() == 3
+    assert loaded_unit.GetDefenseModifier() == 3
+    assert loaded_unit.GetMaxQuantity() == 300
+    assert loaded_unit.GetPrice() == 30    
+    
+    loaded_unit = rm.get_unit_by_id(2)
+    assert loaded_unit.GetName() == "Peasant" # only ids should be unique
+    assert loaded_unit.GetID() == 2
+    assert loaded_unit.GetAttackDice() == 2
+    assert loaded_unit.GetAttackModifier() == 0
+    assert loaded_unit.GetDefenseDice() == 2
+    assert loaded_unit.GetDefenseModifier() == 0
+    assert loaded_unit.GetMaxQuantity() == 100
+    assert loaded_unit.GetPrice() == 50
+
+
+
+def test_load_empty_unit_file (tmp_path):
+    file = tmp_path / "units22.json"
+    rm = ResourceManager()
+    rm.load_unit_types(file)
+    # maybe it should be treated more seriously
+    assert rm.get_unit_types() != None
+    assert len(rm.get_unit_types()) == 0
+    
+
+def test_load_single_building (tmp_path):
+    file = tmp_path / "buildings.json"
+
+    file.write_text("""
+    {
+      "building_types": [
+        {
+          "Nazwa_Budynku": "TestowyBudynek",
+          "Modifier": 2,
+        }
+      ]
+    }
+    """)
+
+    rm = ResourceManager()
+    rm.load_building_types(file)
+    assert rm.get_building_types() != None
+
+    assert len(rm.get_building_types()) == 1
+    
+    loaded_bulding = rm.get_building_types()[0]
+    
+    assert loaded_bulding.Nazwa_Budynku() == "TestowyBudynek"
+    assert loaded_bulding.Modifier == 2
+    
+def test_load_single_terrain (tmp_path):
+    file = tmp_path / "terrain.json"
+
+    file.write_text("""
+    {
+      "terrain_types": [
+        {
+          "Nazwa_terenu": "Pustynia",
+          "Modifier": 2,
+        }
+      ]
+    }
+    """)
+
+    rm = ResourceManager()
+    rm.load_terrain_types(file)
+    assert rm.get_terrain_types() != None
+
+    assert len(rm.get_terrain_types()) == 1
+    
+    loaded_terrain = rm.get_terrain_types()[0]
+    
+    assert loaded_terrain.Nazwa_terenu() == "Pustynia"
+    assert loaded_terrain.Modifier == 2
+    
+    
+    
